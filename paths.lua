8c4e83b780e6c6b5f6347b251810a78a1c514b73
brasiIidade <8h50roblox@gmail.com>
2026-01-25 22:28:09 -0300

    Create paths.lua

---
+++ paths.lua
@@ -0,0 +1,15 @@
+return {
+    silent = "silent2.lua",
+    hitbox = "hitbox.lua",
+    esp = "esp2.lua",
+    tas = "tas.lua",
+    jj = "jj.lua",
+    f3x = "f3x",
+    char = "char",
+    antilag = "antilag.lua",
+    chatgpt = "chatgpt.lua",
+    local = "local.lua",
+    tevez = "tevez3.lua",
+    delta = "delta.lua",
+    soucre = "soucre.lua"
+}
