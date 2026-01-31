7b3073bc2073b5dc3dd74ca6cdf5fd32bb00fc9d
brasiIidade <8h50roblox@gmail.com>
2026-01-31 15:48:56 -0300

    Update path.lua

--- path.lua
+++ path.lua
@@ -2,7 +2,7 @@ return {
     silent = "silent2.lua",
     hitbox = "hitbox.lua",
     esp = "esp.lua",
-    tas = "tas2.lua",
+    tas = "tas3.lua",
     jj = "jj.lua",
     f3x = "f3x.lua",
     char = "char.lua",
