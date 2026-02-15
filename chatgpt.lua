3ed440eb6f703e383fabfdbe3b56e7e398100daf
brasiIidade <8h50roblox@gmail.com>
2026-02-14 23:20:35 -0300

    Update chatgpt.lua

--- chatgpt.lua
+++ chatgpt.lua
@@ -1,10 +1,82 @@
-local REVEAL_HINT_STACK = false
-local ANTI_ENV_LOG_MESSAGE =
-[[
-    skid fudido
-]]
-if not getmetatable or not setmetatable or not type or not select or type(select(2, pcall(getmetatable, setmetatable({}, {__index = function(self, ...) while true do end end})))['__index']) ~= 'function' or not pcall or not debug or not rawget or not rawset or not pcall(rawset,{}," "," ") or getmetatable(require) or getmetatable(print) or getmetatable(error) or ({debug.info(print,'a')})[1]~=0 or ({debug.info(tostring,'a')})[1]~=0 or ({debug.info(print,'a')})[2]~=true or not select or not getfenv or select(1, pcall(getfenv, 69)) == true or not select(2, pcall(rawget, debug, "info")) or #(((select(2, pcall(rawget, debug, "info")))(getfenv, "n")))<=1 or #(((select(2, pcall(rawget, debug, "info")))(print, "n")))<=1 or not (select(2, pcall(rawget, debug, "info")))(print, "s") == "[C]" or not (select(2, pcall(rawget, debug, "info")))(require, "s") == "[C]" or (select(2, pcall(rawget, debug, "info")))((function()end), "s") == "[C]" then
-  return REVEAL_HINT_STACK and tostring(ANTI_ENV_LOG_MESSAGE) or nil
+-- anti
+
+local function Finalizar(Mensagem)
+    print(Mensagem)
+    task.wait(0.5)
+    local function Crash() return Crash() end
+    Crash()
+end
+
+local RanTimes = 0
+local Connection = game:GetService("RunService").Heartbeat:Connect(function()
+    RanTimes = RanTimes + 1
+end)
+
+repeat
+    task.wait()
+until RanTimes >= 2
+
+Connection:Disconnect()
+
+if not getmetatable or not setmetatable or not type or not select or type(select(2, pcall(getmetatable, setmetatable({}, {__index = function(self, ...) while true do end end})))['__index']) ~= 'function' or not pcall or not debug or not rawget or not rawset or not pcall(rawset,{}," "," ") or getmetatable(require) or getmetatable(print) or getmetatable(error) or ({debug.info(print,'a')})[1]~=0 or ({debug.info(tostring,'a')})[1]~=0 or ({debug.info(print,'a')})[2]~=true or not select or not getfenv or select(1, pcall(getfenv, 69)) == true or not select(2, pcall(rawget, debug, "info")) or #(((select(2, pcall(rawget, debug, "info")))(getfenv, "n")))<=1 or #(((select(2, pcall(rawget, debug, "info")))(print, "n")))<=1 or not (select(2, pcall(rawget, debug, "info")))(print, "s") == "[C]" or not (select(2, pcall(rawget, debug, "info")))(require, "s") == "[C]" or (select(2, pcall(rawget, debug, "info")))((function()end), "s") == "[C]" or not select(1, pcall(debug.info, coroutine.wrap(function() end)(), 's')) == false then
+    Finalizar("skid de EB :(")
+end
+
+if not game.ServiceAdded then
+    Finalizar("skid de EB :(")
+end
+
+if getfenv()[Instance.new("Part")] then
+    Finalizar("skid de EB :(")
+end
+
+if getmetatable(__call) then
+    Finalizar("skid de EB :(")
+end
+
+local Success = pcall(function()
+    Instance.new("Part"):BananaPeelSlipper("a")
+end)
+
+if Success then
+    Finalizar("skid de EB :(")
+end
+
+local Success, Result = pcall(function()
+    return game:GetService("HttpService"):JSONDecode([=[
+        [
+            42,
+            "deworming tablets",
+            false,
+            987,
+            true,
+            [555, "shimmer", null],
+            null,
+            ["x", 77, true],
+            {"key": "value", "num": 101},
+            [null, ["nested", 999, false]]
+        ]
+    ]=])
+end)
+
+if not Success then
+    Finalizar("skid de EB :(")
+end
+
+if Result[6][3] ~= nil then
+    Finalizar("skid de EB :(")
+end
+
+local _, Message = pcall(function()
+    game()
+end)
+
+if not Message:find("attempt to call a Instance value") then
+    Finalizar("skid de EB :(")
+end
+
+if #game:GetChildren() <= 4 then
+    Finalizar("skid de EB :(")
 end
 
 local cloneref = cloneref or function(obj) return obj end
