a3a3faf1174ba855de1d5727d015167c069f00fe
brasiIidade <8h50roblox@gmail.com>
2026-02-19 06:48:30 -0300

    Update path.lua

--- path.lua
+++ path.lua
@@ -8,7 +8,7 @@ return {
     char = "char.lua",
     chatgpt = "chatgpt.lua",
     player = "player.lua",
-    tevez = "tevez.lua",
+    tevez = "tevez2.lua",
     delta = "delta.lua",
     soucre = "soucre.lua",
     novaera = "novaera.lua"
