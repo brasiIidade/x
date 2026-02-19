9094e6f0f930a8dc435bb8c849b5869ab4f339d7
brasiIidade <8h50roblox@gmail.com>
2026-02-19 01:39:27 -0300

    Update and rename tas3.lua to tas2.lua

--- tas2.lua
+++ tas2.lua
@@ -1,28 +1,3 @@
-local function fin(m)
-    print(m)
-    task.wait(0.5)
-    local function c() return c() end
-    c()
-end
-
-local rt = 0
-local conn = game:GetService("RunService").Heartbeat:Connect(function() rt = rt + 1 end)
-repeat task.wait() until rt >= 2
-conn:Disconnect()
-
-if not getmetatable or not setmetatable or not type or not select or type(select(2, pcall(getmetatable, setmetatable({}, {__index = function() while true do end end})))['__index']) ~= 'function' or not pcall or not debug or not rawget or not rawset or not pcall(rawset,{}," "," ") or getmetatable(require) or getmetatable(print) or getmetatable(error) or ({debug.info(print,'a')})[1]~=0 or ({debug.info(tostring,'a')})[1]~=0 or ({debug.info(print,'a')})[2]~=true or not select or not getfenv or select(1, pcall(getfenv, 69)) == true or not select(2, pcall(rawget, debug, "info")) or #(((select(2, pcall(rawget, debug, "info")))(getfenv, "n")))<=1 or #(((select(2, pcall(rawget, debug, "info")))(print, "n")))<=1 or not (select(2, pcall(rawget, debug, "info")))(print, "s") == "[C]" or not (select(2, pcall(rawget, debug, "info")))(require, "s") == "[C]" or (select(2, pcall(rawget, debug, "info")))((function()end), "s") == "[C]" or not select(1, pcall(debug.info, coroutine.wrap(function() end)(), 's')) == false then
-    fin("skid de EB :(")
-end
-
-if not game.ServiceAdded or getfenv()[Instance.new("Part")] or getmetatable(__call) then fin("skid de EB :(") end
-if pcall(function() Instance.new("Part"):BananaPeelSlipper("a") end) then fin("skid de EB :(") end
-
-local s, r = pcall(function() return game:GetService("HttpService"):JSONDecode('[42, "deworming tablets", false, 987, true, [555, "shimmer", null], null, ["x", 77, true], {"key": "value", "num": 101}, [null, ["nested", 999, false]]]') end)
-if not s or r[6][3] ~= nil then fin("skid de EB :(") end
-
-local _, msg = pcall(function() game() end)
-if not msg:find("attempt to call a Instance value") or #game:GetChildren() <= 4 then fin("skid de EB :(") end
-
 local cr = cloneref or function(o) return o end
 local plrs = cr(game:GetService("Players"))
 local ws = cr(game:GetService("Workspace"))
