627fbc06116ff3f21f931de5ca2bbf86a730935a
brasiIidade <8h50roblox@gmail.com>
2026-02-21 14:07:04 -0300

    Update path.lua

--- path.lua
+++ path.lua
@@ -4,7 +4,7 @@ return {
     esp = "esp.lua",
     tas = "tas",
     jj = "jj.lua",
-    f3x = "f3x.lua",
+    f3x = "f3x2.lua",
     char = "char.lua",
     chatgpt = "chatgpt.lua",
     player = "player.lua",
