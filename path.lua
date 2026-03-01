d34ab27c311af9e62b297af870ab1422999efaf9
M0vi <you@example.com>
2026-02-28 22:00:21 -0300

    a

--- path.lua
+++ path.lua
@@ -1,7 +1,7 @@
 return {
     silent = "silent.lua",
     treino = "treino.lua",
-    combate = "combate2.lua",
+    combate = "combate3.lua",
     player = "player.lua",
     jogos = "jogos.lua"
 }
